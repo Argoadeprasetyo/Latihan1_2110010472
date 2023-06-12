92329ade287f92e29f88b9ccf653a3757357f051
Argoadeprasetyo <adeargo123@gmail.com>
2023-06-12 20:42:03 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -2,5 +2,14 @@
 package hitung;
 
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //membuat objek
+        Matematika argo = new Matematika(7,2);
+        
+        System.out.println("Hasil penjumlahan: "+argo.setPenjumlahan());
+        System.out.println("Hasil pengurangan: "+argo.setPengurangan());
+        System.out.println("Hasil perkalian: "+argo.setPerkalian());
+        System.out.println("Hasil pembagian: "+argo.setPembagian());
+    }
+
 }
